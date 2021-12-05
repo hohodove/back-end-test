9388c820db597f8ea8dfa117cd2cfba31756e668
hohodove <hohodove@gmail.com>
2021-12-05 23:21:10 +0900

    selectAll(), insert(), update(), delete()を追加。

--- src/main/java/com/test/service/EmployeeService.java
+++ src/main/java/com/test/service/EmployeeService.java
@@ -1,5 +1,7 @@
 package com.test.service;
 
+import java.util.List;
+
 import com.test.entity.Employee;
 import com.test.repository.EmployeeMapper;
 
@@ -12,8 +14,24 @@ public class EmployeeService {
   @Autowired
   private EmployeeMapper employeeMapper;
 
-  public Employee select(Long id){
+  public Employee select(Long id) {
     return employeeMapper.select(id);
+  }
+
+  public List<Employee> selectAll() {
+    return employeeMapper.selectAll();
+  }
+
+  public int insert(Employee employee) {
+    return employeeMapper.insert(employee);
+  }
+
+  public int update(Employee employee) {
+    return employeeMapper.update(employee);
+  }
+
+  public int delete(Long id) {
+    return employeeMapper.delete(id);
   }
 
 }
