452eeda084c5eaada71932a2d60e77e954c7d647
hohodove <hohodove@gmail.com>
2021-12-19 10:51:00 +0900

    [WIP]認証API作成途中

--- src/main/java/com/test/config/SecurityConfig.java
+++ src/main/java/com/test/config/SecurityConfig.java
@@ -1,13 +1,30 @@
 package com.test.config;
 
+
+
+import com.test.Handler.SimpleAccessDeniedHandler;
+import com.test.Handler.SimpleAuthenticationEntryPoint;
+import com.test.Handler.SimpleAuthenticationFailureHandler;
+import com.test.Handler.SimpleAuthenticationSuccessHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @EnableWebSecurity
@@ -29,24 +46,65 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                                "/webjars/**");
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .withUser("yama3")
-        .password(passwordEncoder().encode("123456"))
-        .roles("USER");
+  @Autowired
+  public void configureGlocal(AuthenticationManagerBuilder auth, 
+      @Qualifier("simpleUserDetailsService") UserDetailsService userDetailsService,
+      PasswordEncoder passwordEncoder) throws Exception {
+  
+      auth.eraseCredentials(true)
+          .userDetailsService(userDetailsService)
+          .passwordEncoder(passwordEncoder);
   }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
+        .mvcMatchers("/prelogin", "/greeting")
+            .permitAll()
         .anyRequest()
             .authenticated()
         .and()
-        .formLogin()
+        .exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPoint())
+            .accessDeniedHandler(accessDeniedHandler())
+        .and()
+            .formLogin()
             .loginProcessingUrl("/login").permitAll()
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(authenticationSuccessHandler())
+                .failureHandler(authenticationFailureHandler())
+        .and()
+        .logout()
+            .logoutUrl("/logout")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .logoutSuccessHandler(logoutSuccessHandler())
+        .and()
+        .csrf()
+            //.ignoringAntMatchers("/login")
+            .csrfTokenRepository(new CookieCsrfTokenRepository())
         ;
   }
 
+  AuthenticationEntryPoint authenticationEntryPoint() {
+    return new SimpleAuthenticationEntryPoint();
+  }
+
+  AccessDeniedHandler accessDeniedHandler() {
+    return new SimpleAccessDeniedHandler();
+  }
+
+  AuthenticationSuccessHandler authenticationSuccessHandler() {
+    return new SimpleAuthenticationSuccessHandler();
+  }
+
+  AuthenticationFailureHandler authenticationFailureHandler() {
+    return new SimpleAuthenticationFailureHandler();
+  }
+
+  LogoutSuccessHandler logoutSuccessHandler() {
+    return new HttpStatusReturningLogoutSuccessHandler();
+  }
 
 }
